be9447ff01b5e7128cc4ff6429616fa02e819505
Kristie Peyton <peytonkn@gmail.com>
2018-11-04 17:52:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,3 +29,10 @@ def input_to_index(move)
    index
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  token = current_player(board)
+  
+  
