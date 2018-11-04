9cd03f36da521bc80dcfd373cb1feb9a89d03315
Kristie Peyton <peytonkn@gmail.com>
2018-11-04 17:53:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,4 +35,13 @@ def turn(board)
   index = input_to_index(user_input)
   token = current_player(board)
   
-  
+  if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
+end
